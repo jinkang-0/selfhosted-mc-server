b8976e3f1639acb3c39e94c6d44e584a9346e186
jinkang-0 <jinkang.fang@gmail.com>
2025-10-10 01:55:04 -0700

    initial commit

---
+++ service-sync.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+source ./.env
+
+# obtain instance external IP
+PUBLIC_IP=$(curl https://compute.googleapis.com/compute/v1/projects/${PROJECT_ID}/zones/${ZONE}/instances/${INSTANCE_NAME} -H "Authorization: Bearer $(gcloud auth print-access-token)" | jq '.networkInterfaces[0].accessConfigs[0].natIP')
+
+# update DNS record
+curl -X PATCH "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${DNS_ID}" \
+	-H "Authorization: Bearer ${API_TOKEN}" \
+	-H "Content-Type: application/json" \
+	-d '{"content": '$PUBLIC_IP'}'
+
+# update frpc endpoint
+sed 's/serverAddr = "[^"]+"/serverAddr = "${PUBLIC_IP}"/' ./frp/frpc.toml
+
+# update systemd service
+sudo systemctl daemon-reload
+sudo systemctl restart frp
